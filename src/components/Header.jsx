6d39a3dd3897a22e5eded5febed49dace5f6539d
anastafish <anastafish24@gmail.com>
2023-01-12 16:35:33 +0300

    chnaged img src path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header(){
     return(
         <div className="header-container">
             <div className='header-left'>
-                <img src="../public/images/trollface.png" alt="" />
+                <img src="../images/trollface.png" alt="" />
                 <h1>Meme Generator</h1>
             </div>
             <div>
